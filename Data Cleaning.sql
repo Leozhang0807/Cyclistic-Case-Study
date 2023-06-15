e847c149a785f46b6df541bb6d9aa00aa9ff8208
Leozhang0807 <35789579+Leozhang0807@users.noreply.github.com>
2023-06-15 16:48:01 -0700

    Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -1,33 +1,42 @@
+DROP TABLE IF EXISTS `Tripdata.2022_2023tripdata_combian_cleaned`;
 
-DROP TABLE IF EXISTS `Tripdata.2022_23tripdata_combin_cleaned`;
+CREATE TABLE IF NOT EXISTS `Tripdata.2022_2023tripdata_combian_cleaned` AS (
 
-CREATE TABLE IF NOT EXISTS `Tripdata.2022_23tripdata_combin_cleaned` AS (
-  SELECT T1.ride_id, rideable_type, started_at, ended_at, 
-  start_station_name, end_station_name, start_lat, start_lng, 
-   end_lat, end_lng, member_casual
-  (CASE WHEN day_of_week = 1 THEN 'SUNDAY'
-      WHEN day_of_week = 2 THEN 'MON'
-      WHEN day_of_week = 3 THEN 'TUES'
-      WHEN day_of_week = 4 THEN 'WED'
-      WHEN day_of_week = 5 THEN 'THURS'
-      WHEN day_of_week = 6 THEN 'FRI'
-      WHEN day_of_week = 7 THEN 'SAT' 
-END )AS weekday
+  SELECT T1.ride_id, rideable_type, started_at, ended_at, day_of_week, 
+         start_station_name, start_station_id, end_station_name, end_station_id, 
+         start_lat, start_lng, member_casual, (
+    CASE WHEN day_of_week = 1 THEN 'SUNDAY'
+         WHEN day_of_week = 2 THEN 'MONDAY'
+         WHEN day_of_week = 3 THEN 'TUESDAY'
+         WHEN day_of_week = 4 THEN 'WEDNESDAY'
+         WHEN day_of_week = 5 THEN 'THURSDAY'
+         WHEN day_of_week = 6 THEN 'FRIDAY'
+         WHEN day_of_week = 7 THEN 'SATURDAY'
+      END
+  ) as weekday                                  -- Notice the day of week in the table through Sunday to Saturday
   FROM `Tripdata.2022_23tripdata_combin` AS T1
-  JOIN (
-    SELECT ride_id, 
-  (EXTRACT(HOUR FROM (ended_at - started_at)) * 3600 +
-  EXTRACT(MINUTE FROM (ended_at - started_at))* 60 +
-  EXTRACT(SECOND FROM (ended_at - started_at)))
-  as ride_duration 
-  FROM `Tripdata.2022_23tripdata_combin` 
-  ) AS T2 ON T1.ride_id = T2.ride_id
+  LEFT JOIN(
+    SELECT ride_id, (
+      EXTRACT(HOUR FROM ended_at - started_at) * 3600 +
+      EXTRACT(MINUTE FROM ended_at - started_at) * 60 +
+      EXTRACT(SECOND FROM ended_at - started_at)
+    ) as ride_duration 
+    FROM `Tripdata.2022_23tripdata_combin`
+    
+  ) AS T2
+    ON T1.ride_id = T2.ride_id
+    WHERE 
+      start_station_name IS NOT NULL
+      AND
+      end_station_name IS NOT NULL
+      AND
+      ride_duration > 60
+      AND
+      end_lat IS NOT NULL
+      AND
+      start_lat IS NOT NULL
+)  
+-- Data cleaning remove all null value in the table and rental duration less than 1 minute. 
 
-  WHERE 
-    start_station_name IS NOT NULL AND
-    end_station_name IS NOT NULL AND
-    end_lat IS NOT NULL AND
-    end_lng IS NOT NULL AND
-    ride_duration > 60
-
-)
+select count(*) from `Tripdata.2022_23tripdata_combin`
+-- 4,443,766 rows after data cleaning
