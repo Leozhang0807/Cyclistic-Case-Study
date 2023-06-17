7cb14f3ebf0a4ebc261eb32fb36211655d2ea420
Leozhang0807 <35789579+Leozhang0807@users.noreply.github.com>
2023-06-17 15:55:42 -0700

    Update Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -3,10 +3,9 @@ SELECT COUNT(*) FROM `Tripdata.2022_23tripdata_combin`
 
 -- Before data clean there have 5,859,061 rows data in the database
 
+DROP TABLE IF EXISTS `Tripdata.2022_2023tripdata_combine_cleaned`;
 
-DROP TABLE IF EXISTS `Tripdata.2022_2023tripdata_combian_cleaned`;
-
-CREATE TABLE IF NOT EXISTS `Tripdata.2022_2023tripdata_combian_cleaned` AS (
+CREATE TABLE IF NOT EXISTS `Tripdata.2022_2023tripdata_combine_cleaned` AS (
 
   SELECT T1.ride_id, rideable_type, started_at, ended_at, day_of_week, 
          start_station_name, start_station_id, end_station_name, end_station_id, 
@@ -19,14 +18,15 @@ CREATE TABLE IF NOT EXISTS `Tripdata.2022_2023tripdata_combian_cleaned` AS (
          WHEN day_of_week = 6 THEN 'FRIDAY'
          WHEN day_of_week = 7 THEN 'SATURDAY'
       END
-  ) as weekday                                  -- Covent the day of week in the table through Sunday to Saturday from integer to string
+  ) as weekday,                                     -- Covent the day of week in the table through Sunday to Saturday from integer to string
+  (ended_at - started_at) as ride_duration, more_than_minute                                 
   FROM `Tripdata.2022_23tripdata_combin` AS T1
   LEFT JOIN(
     SELECT ride_id, (
       EXTRACT(HOUR FROM ended_at - started_at) * 3600 +
       EXTRACT(MINUTE FROM ended_at - started_at) * 60 +
       EXTRACT(SECOND FROM ended_at - started_at)
-    ) as ride_duration 
+    ) as more_than_minute 
     FROM `Tripdata.2022_23tripdata_combin`
     
   ) AS T2
@@ -36,13 +36,12 @@ CREATE TABLE IF NOT EXISTS `Tripdata.2022_2023tripdata_combian_cleaned` AS (
       AND
       end_station_name IS NOT NULL
       AND
-      ride_duration > 60
+      more_than_minute > 60
       AND
       end_lat IS NOT NULL
       AND
       start_lat IS NOT NULL
-)  
--- Data cleaning remove all null value in the table and rental duration less than 1 minute. 
+)    -- Data cleaning remove all null value in the table and rental duration less than 1 minute. 
 
 SELECT COUNT(*) FROM `Tripdata.2022_23tripdata_combin`
 -- 4,443,766 rows after data cleaning and there have 1,415,295 been removed
@@ -50,4 +49,5 @@ SELECT COUNT(*) FROM `Tripdata.2022_23tripdata_combin`
 
 ALTER TABLE `Tripdata.2022_2023tripdata_combian_cleaned`
 ADD PRIMARY KEY (ride_id) not enforced
--- Set ride_id as primary key in the database
+-- Set ride_id as primary key in the database 
+  
