d42481f72f9421a39ff48c9b9c608f63c1d2aa4d
Leozhang0807 <35789579+Leozhang0807@users.noreply.github.com>
2023-06-21 17:32:14 -0700

    Update Data Analyst.sql

--- Data Analyst.sql
+++ Data Analyst.sql
@@ -8,4 +8,27 @@ GROUP BY
 ORDER BY 
   member_casual, total_trips ASC
 
+---Number of the ride per month by different member type and ride type
+SELECT CASE 
+  EXTRACT(month FROM started_at) 
+      WHEN 1 THEN 'JAN'
+      WHEN 2 THEN 'FEB'
+      WHEN 3 THEN 'MAR'
+      WHEN 4 THEN 'APR'
+      WHEN 5 THEN 'MAY'
+      WHEN 6 THEN 'JUN'
+      WHEN 7 THEN 'JUL'
+      WHEN 8 THEN 'AUG'
+      WHEN 9 THEN 'SEP'
+      WHEN 10 THEN 'OCT'
+      WHEN 11 THEN 'NOV'
+      WHEN 12 THEN 'DEC'
+      END as month, member_casual, rideable_type, COUNT(*) AS total_trips
+FROM `Tripdata.2022_2023tripdata_combine_cleaned`
+GROUP BY 
+  month, member_casual, rideable_type
+ORDER BY 
+  member_casual, month;
+
+
 
